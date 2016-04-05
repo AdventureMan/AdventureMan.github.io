6f894847496c1c384ca78610d2efdd5debd7d510
Matt <mkeenan20@gmail.com>
2016-04-05 18:26:40 -0400

    removed order receipt; Changes in node server

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ app.get('/summer2016', function (req, res) {
   res.send('You sent the data ' + req.query.songData);
   var mysql = require('mysql');
 
+console.log("listening on port 8080");
+
   var connection = mysql.createConnection({
     host : "10.0.0.32",
     user : "webInput",
@@ -19,15 +21,17 @@ app.get('/summer2016', function (req, res) {
 
   connection.connect();
   var songInput = req.query.songData;
+  var theDate = new Date();
   connection.query('INSERT INTO playlist (songInfo) VALUES (?)', [songInput], function(err, results){
+
     if(err){
       console.log(err);
       console.log("Looks like there was an error");
       return ;
     }
-    console.log("listening on port 8080");
+    console.log("Succesful database entry at" + theDate);
   });
 
-  connection.end();
+  //connection.end();
   //res.redirect('http://www.mattkeenanm.me/summer2016');
 }).listen(8080);
