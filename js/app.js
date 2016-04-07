8555d0d1bc8fc627b719e1866b17e9acd5c2f775
Matt <mkeenan20@gmail.com>
2016-04-06 23:26:45 -0400

    trying to send data over ajax

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var http = require('http');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.mattkeenan.me');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
