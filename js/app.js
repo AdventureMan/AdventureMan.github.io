d3d445dd060baa7f911824bb617962a4aaceb4a3
Matt <mkeenan20@gmail.com>
2016-04-06 23:21:28 -0400

    trying to send data over ajax

--- js/app.js
+++ js/app.js
@@ -4,9 +4,29 @@ var app = express();
 var bodyParser = require('body-parser');
 var http = require('http');
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/summer2016', function (req, res) {
+app.get('/summer2016', function (req, res) {
   res.send('You sent the data: "' + req.query.songData + '"');
   var mysql = require('mysql');
 
@@ -20,7 +40,7 @@ console.log("listening on port 8080");
   });
 
   connection.connect();
-  var songInput = JSON.stringify(req.body);
+  var songInput = req.query.songData;
   var theDate = new Date();
   connection.query('INSERT INTO playlist (songInfo) VALUES (?)', [songInput], function(err, results){
 
